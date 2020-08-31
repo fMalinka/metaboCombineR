e579bc9740900a8549d4650ae740f88a8f4b3758
Frantisek Malinka <frantisek.malinka@gmail.com>
2020-08-31 12:19:22 +0200

    empty rows removed

--- src/main.cpp
+++ src/main.cpp
@@ -176,12 +176,7 @@ std::vector<std::pair<int,int> > metaboCombineR::rtcorrection(alingmentIndexes *
 {
 
     std::vector<std::pair<int,int> > index;
-
-    Rcpp::Rcout << "rtcorrection 1#" << std::endl;
     std::vector<std::string> onerowalign(allAlignments->indexRef.size());
-    std::vector<std::pair<int,int>> pairindex(allAlignments->indexRef.size());
-    Rcpp::Rcout << "inderef: " << allAlignments->indexRef.size() << " nonref: " << allAlignments->indexNonref.size() << std::endl;
-
     std::vector<std::string> a1(allAlignments->indexRef.size());
     std::vector<std::string> a2(allAlignments->indexNonref.size());
 
@@ -198,14 +193,11 @@ std::vector<std::pair<int,int> > metaboCombineR::rtcorrection(alingmentIndexes *
             a1[ial] = allAlignments->expRef->mzStr[allAlignments->indexRef[ial]];
             reals1index[ial] = realindex1;
             ++realindex1;
-            //Rcpp::Rcout << allAlignments->expRef->mzStr[allAlignments->indexRef[ial]] << " ";
         }
         else
         {
             a1[ial] = "-1";
-            onerowalign[ial] = allAlignments->expNonref->mzStr[allAlignments->indexNonref[ial]];
-            //reals1index[ial] = -1;
-            ;//Rcpp::Rcout << "-1 ";
+            onerowalign[ial] = allAlignments->expNonref->mzStr[allAlignments->indexNonref[ial]];            
         }        
     }
 
@@ -219,78 +211,18 @@ std::vector<std::pair<int,int> > metaboCombineR::rtcorrection(alingmentIndexes *
         }
         else
         {
-            //reals2index[ial] = -1;
             a2[ial] = "-1";
         }
     }
 
+    //Rcpp::Function correctClassiqAling("correctClassiqAling");
+    //Rcpp::List listRTcorrected = correctClassiqAling(onerowalign, rtwindowsize);
 
 
-
-    Rcpp::Rcout << "size onerowaling: " << onerowalign.size() << std::endl;
-
-    for(int iii = 0; iii < onerowalign.size(); ++iii)
-    {
-        Rcpp::Rcout << onerowalign[iii] << std::endl;
-    }
-    Rcpp::Rcout << std::endl;
-
-
-   // Rcpp::Rcout << "AAAAAAAAAAAAAAAAA" << std::endl;
-   /* for(int ial = 0; ial < allAlignments->indexNonref.size(); ++ial)
-    {
-        if(allAlignments->indexNonref[ial] != -1)
-        {
-            //onerowalign[ial] = allAlignments->expRef->mzStr[allAlignments->indexRef[ial]];
-            ;//Rcpp::Rcout << allAlignments->expNonref->mzStr[allAlignments->indexNonref[ial]] << " ";
-        }
-        else
-        {
-            ;//Rcpp::Rcout << "-1 ";
-        }
-       // Rcpp::Rcout << allAlignments->indexNonref[ial] << " ";
-       // Rcpp::Rcout << allAlignments->expRef->mzStr[allAlignments->indexRef[ial]] << " ";
-    }
-    */
-    Rcpp::Rcout << std::endl;
-   /*
-    for(int ial = 0; ial < a1.size(); ++ial)
-    {
-        Rcpp::Rcout << a1[ial] << " ";
-    }
-    Rcpp::Rcout << std::endl;
-    for(int ial = 0; ial < a2.size(); ++ial)
-    {
-        Rcpp::Rcout << a2[ial] << " ";
-    }
-
-*/
-    Rcpp::Rcout << std::endl;
-
-    Rcpp::Function correctClassiqAling("correctClassiqAling");
-    Rcpp::List listRTcorrected = correctClassiqAling(onerowalign, rtwindowsize);
-    Rcpp::Rcout << "rtcorrection 2#" << std::endl;
-    Rcpp::CharacterVector xxx0 = listRTcorrected[0];
-    Rcpp::CharacterVector xxx1 = listRTcorrected[1];
-    for(int ii = 0; ii < xxx0.size(); ++ii)
-    {
-        Rcpp::Rcout << "(" << xxx1[ii] << "," << xxx0[ii] << ") ";
-    }
-
-    Rcpp::Rcout << "rtcorrection 4#" << std::endl;
-
-    Rcpp::Rcout << "correcting verssion 2" << std::endl;
     Rcpp::Function correctClassiqAling2("correctClassiqAling2");
     Rcpp::List listRTcorrected2 = correctClassiqAling2(a1, a2, rtwindowsize);
     Rcpp::CharacterVector mz1 = listRTcorrected2[0];
     Rcpp::CharacterVector mz2 = listRTcorrected2[1];
-
-    //std::vector<std::string> reals1index;
-    //std::vector<std::string> reals2index;
-    for(int ii = 0; ii < mz1.size(); ++ii)
-    {
-        Rcpp::Rcout << "(" << mz1[ii] << "," << mz2[ii] << " # " << reals1index[atoi(mz1[ii])-1] << "," << reals2index[atoi(mz2[ii])-1] << ") ";
-    }
 
     //common elements
     for(int icom = 0; icom < allAlignments->indexRef.size(); ++icom)
@@ -298,12 +230,9 @@ std::vector<std::pair<int,int> > metaboCombineR::rtcorrection(alingmentIndexes *
         if(allAlignments->indexRef[icom] != -1 && allAlignments->indexNonref[icom] != -1)
         {
             std::pair<int,int> mypair;
-            //mypair.first = allAlignments->indexRef[icom];
-            //mypair.second = allAlignments->indexNonref[icom];
             mypair.second = allAlignments->indexRef[icom];
             mypair.first = allAlignments->indexNonref[icom];
-            index.push_back(mypair);
-            //Rcpp::Rcout << "first: " << mypair.first << " texxt: " << allAlignments->expRef->mzStr[mypair.first] <<  " second: " << mypair.second << " text: " << allAlignments->expNonref->mzStr[mypair.second] << std::endl;
+            index.push_back(mypair);            
         }
         else if(allAlignments->indexRef[icom] == -1)
         {
@@ -318,168 +247,21 @@ std::vector<std::pair<int,int> > metaboCombineR::rtcorrection(alingmentIndexes *
             mypair.second = allAlignments->indexRef[icom];
             mypair.first = -1;// allAlignments->indexNonref[icom];
             index.push_back(mypair);
-            //Rcpp::Rcout << "n first: " <<allAlignments->indexRef[icom] << " nsecond: " << allAlignments->indexNonref[icom] << std::endl;
         }
     }
 
-    int iindexLast = 0;
     for(int iswap = 0; iswap < mz1.size(); ++iswap)
-    {
-        //mz1 ref
-        //Rcpp::Rcout << "test: " << index[iswap].second << " test2: " << index[iswap].first << std::endl; //ref
-        //Rcpp::Rcout << "ref swap: " << mz1[iswap] << " nonrefswap: " << mz2[iswap]; //ref
-        //Rcpp::Rcout << "z ref swap: " << index[reals1index[atoi(mz1[iswap])-1]].first << " bude: " << index[reals2index[atoi(mz2[iswap])-1]].first << std::endl;
-        Rcpp::Rcout << "index1 real: " << index[atoi(mz1[iswap])-1].second << " index2 real: " << index[atoi(mz1[iswap])-1].second << " index ref: " << index[reals1index[atoi(mz1[iswap])-1]].first << " a " << index[reals1index[atoi(mz1[iswap])-1]].second << " | " << index[reals2index[atoi(mz2[iswap])-1]].first << " a " << index[reals2index[atoi(mz2[iswap])-1]].second  << std::endl;
-
+    {        
          index[atoi(mz1[iswap])-1].first = index[atoi(mz2[iswap])-1].first;
          index[atoi(mz2[iswap])-1].first = -1;
-         Rcpp::Rcout << "bla: " << index[atoi(mz2[iswap])-1].first << " bla2: " << index[atoi(mz2[iswap])-1].second << std::endl;
-        //Rcpp::Rcout << "reference: " << index[atoi(mz1[iswap])-1].first << " a " << index[atoi(mz1[iswap])-1].second << " non-reference: " << index[atoi(mz2[iswap])-1].first << " a " << index[atoi(mz2[iswap])-1].second << std::endl;
-
-        //reference
-
-
-        //reals1index[atoi(mz1[iswap])-1];
-        //nonreference
-        //reals2index[atoi(mz2[iswap])-1];
-
-        //index[reals1index[atoi(mz1[iswap])-1]].first = index[reals2index[atoi(mz2[iswap])-1]].first;
-        //index[reals2index[atoi(mz2[iswap])-1]].first = -1;
-        //index[atoi(mz2[iswap])].first = -1;
-        //mz2[iswap]; //nonref
     }
 
     std::vector<std::pair<int,int> > finalindex;
     for(int ii = 0; ii < index.size(); ++ii)
     {
-        Rcpp::Rcout << "A: " << index[ii].first << " B: " << index[ii].second << std::endl;
-        //if(index[ii].first != -1 && index[ii].second != -1)
-        //{
-            finalindex.push_back(index[ii]);
-        //}
-        //if(index[ii].first != -1 && index[ii].first ==
-        ;//index[ii].second;
+        finalindex.push_back(index[ii]);
     }
     return finalindex;
-    //            std::vector<std::pair<int,int> > resultsInds;   //nonref, ref
-    //            return resultsInds;
-
-
-    //alignment index to sequence index
-
-
-    /*
-    std::vector<std::pair<int,int> > resultsInds;   //nonref, ref
-    std::vector<std::vector<int> > commonIndexesRefCandidates((*allAlignments)[0].expRef->mz.size());
-    std::vector<int> commonIndexesRef((*allAlignments)[0].expRef->mz.size(), -1);
-
-    for(int iali = 0; iali < allAlignments->size(); ++iali)
-    {
-        for(int irow = 0; irow < (*allAlignments)[iali].indexRef.size(); ++irow)
-        {
-            if((*allAlignments)[iali].indexRef[irow] != -1 && (*allAlignments)[iali].indexNonref[irow] != -1)
-            {
-                commonIndexesRef[(*allAlignments)[iali].indexRef[irow]] = (*allAlignments)[iali].indexNonref[irow]+(*allAlignments)[iali].startRow;
-                commonIndexesRefCandidates[(*allAlignments)[iali].indexRef[irow]].push_back((*allAlignments)[iali].indexNonref[irow]+(*allAlignments)[iali].startRow);
-            }
-
-        }
-    }
-    for(int iref = 0; iref < commonIndexesRefCandidates.size(); ++iref)
-    {
-        if(!commonIndexesRefCandidates[iref].empty())
-        {
-            std::map<std::string,std::vector<int> > majorityVote; //mz value, commonIndexesRef
-            for(int icand = 0; icand < commonIndexesRefCandidates[iref].size(); ++icand)
-            {
-                majorityVote[(*allAlignments)[0].expNonref->rtStr[commonIndexesRefCandidates[iref][icand]]].push_back(commonIndexesRefCandidates[iref][icand]);
-            }
-            //look for votes
-            std::map<std::string,std::vector<int> >::iterator it;
-
-            std::string bestCand;
-            int bestCandvotes = 0;
-            double mindistance = 100000;
-            for (it = majorityVote.begin(); it != majorityVote.end(); it++)
-            {
-                if(it->second.size() > bestCandvotes)
-                {
-                    bestCandvotes = it->second.size();
-                    bestCand = it->first;
-                    mindistance = std::abs((*allAlignments)[0].expRef->rt[iref] - (*allAlignments)[0].expNonref->rt[it->second[0]]);
-                }
-                else
-                {
-                    if(it->second.size() == bestCandvotes)
-                    {
-                        double rtdist = std::abs((*allAlignments)[0].expRef->rt[iref] - (*allAlignments)[0].expNonref->rt[it->second[0]]); //myact2->expNonref->rt[it->second[0]]);
-                        if(rtdist < mindistance)
-                        {
-                            bestCandvotes = it->second.size();
-                            bestCand = it->first;
-                            mindistance = std::abs((*allAlignments)[0].expRef->rt[iref] - (*allAlignments)[0].expNonref->rt[it->second[0]]);
-                        }
-                    }
-                }
-
-            }
-            commonIndexesRef[iref] = majorityVote[bestCand][0];
-        }
-    }
-    std::vector<int> missingNonref(commonIndexesRef.size(), -1);
-    std::map<int, bool> missingNonrefOccurance;
-    for(int ires = 0; ires < commonIndexesRef.size(); ++ires)
-    {
-        if(commonIndexesRef[ires] != -1)
-        {
-            missingNonref[commonIndexesRef[ires]] = 1;
-            missingNonrefOccurance[commonIndexesRef[ires]] = true;
-        }
-    }
-
-    std::vector<int> missingNonrefInd;
-    for(int ic = 0; ic < (*allAlignments)[0].expNonref->mzStr.size(); ++ic)
-    {
-        if(missingNonrefOccurance.find(ic) == missingNonrefOccurance.end())
-        {
-            missingNonrefInd.push_back(ic);
-        }
-    }
-    int mnmislastid = 0;//0;
-    for(int ires = 0; ires < commonIndexesRef.size(); ++ires)
-    {
-        if(commonIndexesRef[ires] != -1)
-        {
-            //only on nonref
-            if(!missingNonrefInd.empty())
-            {
-                if(mnmislastid < (missingNonrefInd.size()))
-                {
-                    while(missingNonrefInd[mnmislastid] < commonIndexesRef[ires])
-                    {
-
-                        resultsInds.push_back(std::pair<int,int>(missingNonrefInd[mnmislastid], -1));
-                        mnmislastid++;
-                        if(mnmislastid >= missingNonrefInd.size())
-                            break;
-                    }
-                }
-            }
-
-            //common
-            resultsInds.push_back(std::pair<int,int>(commonIndexesRef[ires], ires));
-        }
-        else
-        {
-            resultsInds.push_back(std::pair<int,int>(-1, ires));
-        }
-    }
-    for(int ires = mnmislastid; ires < missingNonrefInd.size(); ++ires)
-    {
-        resultsInds.push_back(std::pair<int,int>(missingNonrefInd[ires], -1));
-    }
-    return resultsInds;
-    */
 
 }
 
@@ -600,67 +382,27 @@ experiment metaboCombineR::generateAlignmentExperiment(int window_size, experime
 //functiont that combines two alignments into the final experiment -RT
 experiment metaboCombineR::generateAlignmentExperimentRT(int window_size, experiment *expNonRef, experiment *exp2Ref)
 {
-    //std::vector<std::vector<std::string > > mysamples_windows;
-    //for(int istart = window_size-1; istart < expNonRef->mz.size(); ++istart)
-    //{
-    //    std::vector<std::string > actSample;
-    //    actSample.resize(window_size);
-    //    for(int iwin = 0; iwin < window_size; ++iwin)
-    //    {
-    //       actSample[(window_size-1)-iwin] = expNonRef->mzStr[istart-iwin];
-    //    }
-    //    mysamples_windows.push_back(actSample);
-    //}
+
+    std::list<std::string *> nonrefmzStr;
+    for(int il = 0; il < expNonRef->mzStr.size(); ++il)
+    {
+        nonrefmzStr.push_back(&(expNonRef->mzStr[il]));
+    }
 
 
-    //std::vector<alingmentIndexes> allAlignments;
-    //for(int isample = 0; isample < mysamples_windows.size(); ++isample)
-    //{
-        //std::list<std::string*> rowsrc_profile_mz;
-        //for(unsigned long irow = 0; irow < mysamples_windows[isample].size(); ++irow)
-        //{
-        //    rowsrc_profile_mz.push_back(&mysamples_windows[isample][irow]);
-        //}
-        std::list<std::string *> nonrefmzStr;
- //       Rcpp::Rcout << "FIRST:" << std::endl;
-        for(int il = 0; il < expNonRef->mzStr.size(); ++il)
-        {
-            nonrefmzStr.push_back(&(expNonRef->mzStr[il]));
- //           Rcpp::Rcout << *(nonrefmzStr.back()) << " ";
-        }
-        Rcpp::Rcout << std::endl;
-//         Rcpp::Rcout << "SECOND:" << std::endl;
-        for(int il = 0; il < exp2Ref->mzStr.size(); ++il)
-        {
-            //nonrefmzStr.push_back(&(expNonRef->mzStr[il]));
-//            Rcpp::Rcout << exp2Ref->mzStr[il] << " ";
-        }
-// Rcpp::Rcout << std::endl;
+    mySolution rowAlign = computeSimilarityMatrix4Complete((nonrefmzStr), &(exp2Ref->mzStr));//computeSimilarityMatrix4CompleteSemiglobal((nonrefmzStr), &(exp2Ref->mzStr));
+    //mySolution rowAlign = computeSimilarityMatrix4CompleteSemiglobal((nonrefmzStr), &(exp2Ref->mzStr));
+    std::string file= "myfile.log";
+    printSemiAlignmentToFile(&rowAlign, &(exp2Ref->mzStr), file);
 
-        Rcpp::Rcout << "1#" << std::endl;
-        mySolution rowAlign = computeSimilarityMatrix4Complete((nonrefmzStr), &(exp2Ref->mzStr));//computeSimilarityMatrix4CompleteSemiglobal((nonrefmzStr), &(exp2Ref->mzStr));
-        //mySolution rowAlign = computeSimilarityMatrix4CompleteSemiglobal((nonrefmzStr), &(exp2Ref->mzStr));
-        std::string file= "myfile.log";
-        printSemiAlignmentToFile(&rowAlign, &(exp2Ref->mzStr), file);
-        Rcpp::Rcout << "2#" << std::endl;
-        Rcpp::Rcout << "scorerow: " << rowAlign.score << std::endl;
-
-        //NAHRADIT SEMIGLOBAL rekonstrukci!!!!!!!!
-        alingmentIndexes resAlign = processSemiAlignment(&rowAlign, &(exp2Ref->mzStr));
+    //NAHRADIT SEMIGLOBAL rekonstrukci!!!!!!!!
+    alingmentIndexes resAlign = processSemiAlignment(&rowAlign, &(exp2Ref->mzStr));
+    resAlign.expNonref = expNonRef;
+    resAlign.expRef = exp2Ref;
+    resAlign.startRow = 0;
 
 
-
-        //      alingmentIndexes resAlign;
-        resAlign.expNonref = expNonRef;
-        resAlign.expRef = exp2Ref;
-        resAlign.startRow = 0;
-        //allAlignments.push_back(resAlign);
-    //}
-    //std::vector<std::pair<int,int> > index2table = aggregateAlignments(&allAlignments); //nonref, ref
-
- Rcpp::Rcout << "3#" << std::endl;
     std::vector<std::pair<int,int> > index2table = rtcorrection(&resAlign, window_size); //nonref, ref
-     Rcpp::Rcout << "4#" << std::endl;
 
     Rcpp::NumericMatrix mat(index2table.size(), exp2Ref->experiments.ncol() + expNonRef->experiments.ncol());
     Rcpp::CharacterVector chmat(index2table.size());
@@ -726,10 +468,13 @@ experiment metaboCombineR::generateAlignmentExperimentRT(int window_size, experi
         newRTstr[irow] = rt2str.str();
     }
     //add secondly reftable
+    int matnewsize = 0;
     for(int irow = 0; irow < index2table.size(); ++irow)
     {
         if(index2table[irow].first == -1 && index2table[irow].second == -1)
             continue;
+        ++matnewsize;
+
         for(int icol = expNonRef->experiments.ncol(); icol < mat.ncol(); ++icol)
         {
             if(index2table[irow].second == -1)
@@ -742,19 +487,39 @@ experiment metaboCombineR::generateAlignmentExperimentRT(int window_size, experi
             chmat(irow) +=  std::string("/") + exp2Ref->rtStr[index2table[irow].second];
     }
 
+     Rcpp::NumericMatrix mat2(matnewsize, exp2Ref->experiments.ncol() + expNonRef->experiments.ncol());
+     Rcpp::CharacterVector chmat2(matnewsize);
+     std::vector<std::string> newRTstr2(matnewsize);
+     Rcpp::NumericVector newRT2(matnewsize);
+     std::vector<std::string> newMZstr2(matnewsize);
+     Rcpp::NumericVector newMZ2(matnewsize);
 
-    Rcpp::colnames(mat) = chcolnames;
-    experiment toReturn;
-    toReturn.experiments = mat;
-    toReturn.mz = newMZ;
-    toReturn.mzStr = newMZstr;
-    toReturn.rownames = chmat;
-    toReturn.rt = newRT;
-    toReturn.rtStr = newRTstr;
-    toReturn.colnames = chcolnames;
+     int inewrow = 0;
+     for(int irow = 0; irow < index2table.size(); ++irow)
+     {
+         if(index2table[irow].first != -1 || index2table[irow].second != -1)
+         {
+             chmat2[inewrow] = (chmat(irow));
+             mat2(inewrow, Rcpp::_) = mat(irow, Rcpp::_);
+             newRTstr2[inewrow] = newRTstr[irow];
+             newRT2[inewrow] = newRT2[irow];
+             newMZstr2[inewrow] = newMZstr[irow];
+             newMZ2[inewrow] = newMZ[irow];
+            ++inewrow;
+         }
+     }
 
+     Rcpp::colnames(mat2) = chcolnames; //ok
+     Rcpp::rownames(mat2) = chmat2;
+     experiment toReturn;
+     toReturn.experiments = mat2;   //ok
+     toReturn.mz = newMZ2;
+     toReturn.mzStr = newMZstr2;
+     toReturn.rownames = chmat2;    //ok
+     toReturn.rt = newRT2;  //ok
+     toReturn.rtStr = newRTstr2;    //ok
+     toReturn.colnames = chcolnames;    //ok
 
-    //experiment toReturn;
     return toReturn;
 }
 
